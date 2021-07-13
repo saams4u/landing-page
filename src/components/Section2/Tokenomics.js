049a1d2d4e5c58ad80f2c7e39ffb86a95550b2a8
Nicholas Vale <nickeev@gmail.com>
2021-07-13 13:54:20 -0700

    Supply

--- src/components/Section2/Tokenomics.js
+++ src/components/Section2/Tokenomics.js
@@ -18,9 +18,9 @@ const Tokenomics = () =>
 
         {/* Title Image & Description */}
         <img id='tokenomics' className='mt-64 mb-7 self-start z-20' src={tokenomicsTextImage} alt='Tokenomics' data-aos="fade"/>
-        <h3 className='font-VT323 mb-28 lg:mb-16 text-2xl z-20 self-start z-20' data-aos="fade">Our tokenomics breakdown. Total Supply: <a className="underline text-brightyellow" href={Links.projectProposal}>100M RENT Tokens</a></h3>
+        <h3 className='font-VT323 mb-28 lg:mb-16 text-2xl z-20 self-start z-20' data-aos="fade">Our token distribution breakdown. Full tokenomics to be released.</h3>
 
-        <div className='inline-flex mb-56 flex-wrap lg:flex-nowrap justify-center z-20'>       
+        <div className='inline-flex mb-56 flex-wrap lg:flex-nowrap justify-center z-20'>    
 
             {/* Coin Circle Image */}
             <img className='object-contain mr-0 lg:mr-24 mb-24 lg:mb0' src={circle} data-aos="flip-right" data-aos-anchor-placement='center-bottom' data-aos-duration="900" />
